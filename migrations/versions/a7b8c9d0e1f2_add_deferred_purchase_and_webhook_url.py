1ea73fb1f2506ada86e5e7847635b6ca0a1eefd9
hridoyhossain100100 <134464434+hridoyhossain100100@users.noreply.github.com>
2026-05-16 09:16:56 +0600

    fix: correct migration chain order

--- migrations/versions/a7b8c9d0e1f2_add_deferred_purchase_and_webhook_url.py
+++ migrations/versions/a7b8c9d0e1f2_add_deferred_purchase_and_webhook_url.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'a7b8c9d0e1f2'
-down_revision: Union[str, None] = '2346a5d9e53f'
+down_revision: Union[str, None] = '6f95f5ca1ac2'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
